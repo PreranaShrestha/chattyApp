145ee5e06c83eced9e15c3338d51bfd01acc6930
PreranaShrestha <prerana.sh@gmail.com>
2018-02-23 21:31:57 +0000

    Get rid of warning

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,8 @@ class ChatBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: "Anonymous"
+      currentUser: "Anonymous",
+      messageText: ''
     };
   }
 
